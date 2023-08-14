188cbafa9f56b3f4322242fa36d53b63c97100ef
CFIT Corporation <80238089+CFITCorporation@users.noreply.github.com>
2023-08-14 13:50:24 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,99 @@
+- ## **WaveDance - CFIT示波器**
+### **前言**
+**互联网的万千矿藏里，
+一首音乐被我发掘。
+古老电音中节奏动感，
+而它的波形更让人神迷。
+立方体旋转于二维世界，
+熟悉的余弦由疏向密。
+华丽的图案，
+却存活于示波器，
+怎不徒生遗憾？
+发掘wav的奥秘，
+网页——
+正是音频的跃动。**
+**这首音乐，正是Oscillofunwave，本项目由此问世。[（由此下载该音频，可用于测试）](https://raw.gitmirror.com/CFITCorporation/cfitpic/main/oscillofun-wave.wav "由此下载该音频")
+[演示视频由此查看](https://www.bilibili.com/video/BV1yz4y1v73L "演示视频由此查看")**
+![wave01](https://img.suze666.top/i/2023/08/14/64d9b1610242b.png)
+![wave02](https://img.suze666.top/i/2023/08/14/64d9b16239650.png)
+****
+### **简介**
+**本项目由CFIT(CIT)编写，基于HTML5、JavaScript的FileReader对象以及Canvas属性实现。**
+****
+### **原理**
+**主函数`change()`中核心部分如下：**
+```javascript
+var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function() {
+            var dur;
+            dur = isNaN(aud.duration) == true ? 1 : aud.duration;//无效音频文件默认长度1
+            total.innerHTML = exchange(parseInt(dur));
+            bar.max = dur;
+            var audioData = reader.result;
+            audioContext.decodeAudioData(audioData, buffer => {
+                drawWaveform(buffer);//获取双声道及显示效果
+            });
+        }
+```
+**用户载入文件时，建立FileReader对象，确认音频长度，将二进制音频数据解码成可用的音频缓冲区。
+`drawWaveform(buffer)`根据解码后的音频缓冲区数据绘制波形图。**
+
+**波形绘制`drawWaveform`核心部分如下：**
+```javascript
+var i0 = 0,
+    j0 = 0,
+    cyl = 847; //每秒展示点数
+datax = buffer.getChannelData(0); //双声道-x
+datay = buffer.getChannelData(1); //双声道-y
+/*省略部分代码*/
+/*以下代码在一个定时器执行*/
+if (j0 + cyl < Math.min(datax.length, datay.length)) {
+    j0 = j0 + cyl; //更新进度
+} else {
+    i0 = 0;
+    j0 = 0;
+}
+for (var i = i0; i < j0; i++) {
+    var x = (1 + datax[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + width / 2;
+    var y = (1 + datay[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + height / 2;
+    if (i == 0) {
+        canvasContext.moveTo(width - x, y);
+    } else {
+        canvasContext.fillRect(width - x, y, 1.5, 1.5);
+        //canvasContext.lineTo(width-x, y);
+    }
+}
+i0 = j0;
+/**/
+```
+**该部分代码实现波形图的绘制逻辑。**
+
+**代码先定义三个核心变量：**
+- **`i0`表示当前展示的波形图起始点的索引位置。**
+- **`j0`表示当前展示的波形图结束点的索引位置。**
+- **`cyl`表示每秒展示的数据点数。**
+
+**datax、datay从音频缓冲区中获取双声道-x和双声道-y的数据：**
+**在定时器回调函数内部的逻辑如下：**
+-  **检查`j0 + cyl`是否小于等于双声道数据的最小长度（即判断是否还有足够的数据可以展示）。**
+- **如果满足条件，将`j0`更新为`j0 + cyl`，以便继续展示下一个时间段的数据；否则，重置`i0`和`j0`为0，重新开始展示波形图。**
+-  **使用循环遍历从`i0`到`j0`之间的数据点，计算每个数据点在Canvas中的坐标（x和y）：**
+   - **`x`的计算公式是 `(1 + datax[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + width / 2`，表示根据数据值确定x坐标位置。**
+   - **`y`的计算公式是 `(1 + datay[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + height / 2`，表示根据数据值确定y坐标位置。**
+-  **在循环中，根据当前点的位置，使用Canvas绘制函数（`fillRect`）在波形图上绘制一个小方块。**
+**由此不断更新`i0`为`j0`，实现展示波形图的数据，最终实现音频波形图的展示效果。**
+****
+### **使用方法**
+- **打开网页，自动展示控制台。**
+![01](https://img.suze666.top/i/2023/08/14/64d9bd90b2e1f.png)
+- **点击`文件载入`，载入文件。**
+![02](https://img.suze666.top/i/2023/08/14/64d9bdf688848.png)
+- **点击确定，即启动示波器。启动速度与文件大小有关，建议文件不要超过100MB,可能无法播放音频。₁**
+![03](https://img.suze666.top/i/2023/08/14/64d9bea26729b.png)
+**₂**
+****
+### **注意事项**
+**₁ 文件过大，浏览器可能崩溃；同时，经过研究，发现当音频大于100MB时，代码行`readers.readAsDataURL(file)`返回值可能为空，无法播放。**
+**₂ 本项目效果及函数均在浏览器进行，故效果优劣与使用浏览器性能有关。**
+****
---
+++ cfitwave.js
@@ -0,0 +1,249 @@
+/*
+CFITWave v1.0
+(c) 2023 CFIT
+https://github.com/CFITCorporation
+*/
+var fileInput = document.getElementById('fileInput');
+var aud = document.getElementById('aud');
+var bar = document.getElementById('bar');//进度条
+var dragg = true;//保证拖动进度条时不受进度条进度的影响
+var namemusic = document.getElementById('namemusic');//获取载入文件名称
+var length = document.getElementById('length');//获取载入文件长度
+var total = document.getElementById('total');//获取载入文件长度（时间格式化）
+var tooldiv = document.getElementById('tooldiv');//控制台界面
+var audioContext = new AudioContext();
+var waveformCanvas = document.getElementById('waveformCanvas');//示波器画布
+var canvasContext = waveformCanvas.getContext('2d');
+waveformCanvas.height = window.innerHeight;
+waveformCanvas.width = window.innerWidth;
+var width = waveformCanvas.width;
+var height = waveformCanvas.height;
+canvasContext.beginPath();
+canvasContext.fillStyle = 'lime';
+canvasContext.beginPath();
+//横竖线
+canvasContext.setLineDash([1, 1]);
+canvasContext.lineWidth = 0.4;
+canvasContext.moveTo(width / 2 - Math.min(height, width) / 2, Math.min(height, width) / 2);
+canvasContext.lineTo(width / 2 - Math.min(height, width) / 2 + Math.min(height, width), Math.min(height, width) / 2);
+canvasContext.strokeStyle = "#cccccc";
+canvasContext.stroke();
+
+canvasContext.setLineDash([1, 1]);
+canvasContext.beginPath();
+canvasContext.lineWidth = 0.4;
+canvasContext.moveTo(width / 2 - Math.min(height, width) / 2 + Math.min(height, width) / 2, 0);
+canvasContext.lineTo(width / 2 - Math.min(height, width) / 2 + Math.min(height, width) / 2, Math.min(height, width));
+canvasContext.strokeStyle = "#cccccc";
+canvasContext.stroke();
+/*进度条移动*/
+function changeprogress() {
+    dragg = false;
+    length.innerHTML = exchange(bar.value);
+    aud.currentTime = (bar.value / bar.max) * aud.duration;
+    dragg = true;
+}
+var times = 0,
+    method = "show";
+setTimeout(function() {
+    shoconsole();//展示控制台
+}, 500);
+/*判断控制台展示与否*/
+function shohid() {
+    times = times + 1;
+    times % 2 == 0 ? hidconsole() : shoconsole();
+}
+/*隐藏控制台*/
+function hidconsole() {
+    method = 'hide';
+    let hid = anime({
+        targets: ".tooldiv",
+        translateY: [-tooldiv.clientHeight + 30],
+        duration: 2000
+    });
+}
+/*展示控制台*/
+function shoconsole() {
+    method = "show";
+    let hid = anime({
+        targets: ".tooldiv",
+        translateY: [0],
+        duration: 2000
+    });
+}
+/*窗体移动时对横纵线的调整*/
+function resizes() {
+    width = waveformCanvas.width;
+    height = waveformCanvas.height;
+    waveformCanvas.height = window.innerHeight;//重置高
+    waveformCanvas.width = window.innerWidth;//重置宽
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'lime';
+    canvasContext.beginPath();
+    canvasContext.setLineDash([1, 1]);
+    canvasContext.lineWidth = 0.4;
+    canvasContext.moveTo(width / 2 - Math.min(height, width) / 2, height / 2);
+    canvasContext.lineTo(width / 2 - Math.min(height, width) / 2 + Math.min(height, width), height / 2);
+    canvasContext.strokeStyle = "#cccccc";
+    canvasContext.stroke();
+    canvasContext.setLineDash([1, 1]);
+    canvasContext.beginPath();
+    canvasContext.lineWidth = 0.4;
+    canvasContext.moveTo(width / 2, height / 2 - Math.min(height, width) / 2);
+    canvasContext.lineTo(width / 2, height / 2 - Math.min(height, width) / 2 + Math.min(height, width));
+    canvasContext.strokeStyle = "#cccccc";
+    canvasContext.stroke();
+}
+window.onresize = function() {
+    resizes();
+}
+resizes();
+aud.onended = function() {
+    change();//结束，再来一遍
+}
+var docElm = document.body;
+docElm.onfullscreenchange = function() {
+    if (document.fullscreenElement) {
+        tooldiv.style.display = "none";
+    } else {
+        tooldiv.style.display = "block";
+    }
+}
+
+function fc() {//全屏
+    if (docElm.requestFullscreen) {
+        docElm.requestFullscreen();
+    } else
+    if (docElm.msRequestFullscreen) {
+        docElm.msRequestFullscreen();
+    } else
+    if (docElm.mozRequestFullScreen) {
+        docElm.mozRequestFullScreen();
+    } else
+    if (docElm.webkitRequestFullScreen) {
+        docElm.webkitRequestFullScreen();
+    }
+}
+/*单击全屏*/
+waveformCanvas.onclick = function() {
+    fc();
+}
+/*主函数*/
+function change() {
+    width = waveformCanvas.width;
+    height = waveformCanvas.height;
+    canvasContext.clearRect(0, 0, width, height);
+    var file = fileInput.files[0];
+    var readers = new FileReader();
+    readers.readAsDataURL(file);
+    console.log(file.name);
+    namemusic.innerHTML = file.name;
+    readers.onload = function() {
+        aud.src = readers.result;
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function() {
+            var dur;
+            dur = isNaN(aud.duration) == true ? 1 : aud.duration;//无效音频文件默认长度1
+            total.innerHTML = exchange(parseInt(dur));
+            bar.max = dur;
+            var audioData = reader.result;
+            audioContext.decodeAudioData(audioData, buffer => {
+                drawWaveform(buffer);//获取双声道及显示效果
+            });
+        }
+    }
+}
+/*实时更新进度条*/
+aud.ontimeupdate = function() {
+    if (dragg == true) {
+        bar.value = parseInt(aud.currentTime);
+        length.innerHTML = exchange(parseInt(aud.currentTime));
+    }
+}
+/*格式化时间*/
+function exchange(index) {
+    var s = index % 60;
+    var m = parseInt(index / 60);
+    var mch = m < 10 ? '0' + m.toString() : m.toString();
+    var sch = s < 10 ? '0' + s.toString() : s.toString();
+    var result = mch + ':' + sch;
+    return result;
+}
+/*接收文件并执行主函数*/
+fileInput.addEventListener('change', () => {
+    change();
+});
+/*调整音频进度时，更新示波进度*/
+aud.onseeked = function() {
+    var dur = isNaN(aud.duration) == true ? 1 : aud.duration;
+    j0 = parseInt((aud.currentTime / dur) * Math.min(datax.length, datay.length));
+}
+var ttt, tz;
+var i0 = 0,
+    j0 = 0,
+    cyl = 847;//每秒展示点数
+var datax, datay;
+/*获取双声道及显示效果*/
+function drawWaveform(buffer) {
+    datax = buffer.getChannelData(0);//双声道-x
+    datay = buffer.getChannelData(1);//双声道-y
+    var width;
+    var height;
+    width = waveformCanvas.width;
+    height = waveformCanvas.height;
+
+    if (typeof(ttt) != 'undefined') {
+        clearInterval(ttt);
+        canvasContext.clearRect(0, 0, width, height);
+        canvasContext.beginPath();
+        canvasContext.strokeStyle = 'lime';
+    }
+    if (typeof(tz) != 'undefined') {
+        clearInterval(tz);
+    }
+    canvasContext.clearRect(0, 0, width, height);
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = 'green';
+    aud.play();
+    i0 = 0;
+    j0 = 0;
+    var dur;
+    dur = isNaN(aud.duration) == true ? 1 : aud.duration;
+    tz = setInterval(function() {//每隔5s调整音频进度，减少与示波器的误差
+        dur = isNaN(aud.duration) == true ? 1 : aud.duration;
+        aud.currentTime = (i0 / Math.min(datax.length, datay.length)) * dur;
+    }, 5000);
+    hidconsole();
+    times = times + 1;
+    ttt = setInterval(function() {
+        dur = isNaN(aud.duration) == true ? 1 : aud.duration;
+        bar.max = dur;
+        total.innerHTML = exchange(parseInt(dur));
+        width = waveformCanvas.width;
+        height = waveformCanvas.height;
+        canvasContext.clearRect(0, 0, width, height);
+        resizes();
+        canvasContext.beginPath();
+        canvasContext.fillStyle = 'lime';
+        canvasContext.beginPath();
+        if (j0 + cyl < Math.min(datax.length, datay.length)) {
+            j0 = j0 + cyl;//更新进度
+        } else {
+            i0 = 0;
+            j0 = 0;
+        } //clearInterval(ttt);}  
+        for (var i = i0; i < j0; i++) {
+            var x = (1 + datax[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + width / 2;
+            var y = (1 + datay[i]) / 2 * Math.min(height, width) - Math.min(height, width) / 2 + height / 2;
+            if (i == 0) {
+                canvasContext.moveTo(width - x, y);
+            } else {
+                canvasContext.fillRect(width - x, y, 1.5, 1.5);
+                //canvasContext.lineTo(width-x, y);
+            }
+        }
+        i0 = j0;
+        canvasContext.stroke();
+    }, (Math.sin(i0 / cyl) + 1) * 1 / 2 + 17.59);//玄学Interval，可改动
+}
